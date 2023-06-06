66a433094652840b609a20ecf7424e3c6914a7dc
Eugene1910 <evgeniy.danilych@mail.ru>
2023-06-06 23:26:47 +0300

    добавили solution

---
+++ solution/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая из
+//  имеющегося массива строк формирует 
+//  массив из строк, длина которых меньше либо
+//  равна 3 символа.
+
+string[] array1 = new string[5] {"564", "13", "597462", "c#", "result"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
